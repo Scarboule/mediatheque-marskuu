dc126952674f74e1465596621aacf89a1d1536f5
Lucas Charoing <charoing.lucas@gmail.com>
2023-02-24 14:44:03 +0100

    Add fade animation to affiliate input

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ form.addEventListener('submit', (e) => {
 })
 console.log(arrow.style.rotate)
 affiliateLabel.addEventListener('click', (e) => {
-    affiliateLabel.classList.toggle('ml-0');
-    affiliateLabel.classList.toggle('ml-4');
-    if (arrow.style.rotate) arrow.style.rotate = null;
-    else arrow.style.rotate = '90deg';
     if (affiliateInput.style.opacity) {
         affiliateInput.style.opacity = null;
         affiliateInput.style.zIndex = null;
+        affiliateLabel.style.translate = null;
+        arrow.style.rotate = null;
     } else {
         affiliateInput.style.opacity = '1';
         affiliateInput.style.zIndex = '1';
+        affiliateLabel.style.translate = '1rem 0';
+        arrow.style.rotate = '90deg';
     }
 })
